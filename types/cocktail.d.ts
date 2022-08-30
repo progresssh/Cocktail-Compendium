97ffc0be8f113faf61e305dc8a95e9e600f74303
Paul <paul.vandamme@outlook.fr>
2022-08-30 16:22:27 +0200

    Update cocktail.d.ts

--- types/cocktail.d.ts
+++ types/cocktail.d.ts
@@ -20,36 +20,36 @@ export interface Drink {
   'strInstructionsZH-HANS': null
   'strInstructionsZH-HANT': null
   strDrinkThumb: string
-  strIngredient1: string
-  strIngredient2: string
-  strIngredient3: string
+  strIngredient1: null | string
+  strIngredient2: null | string
+  strIngredient3: null | string
   strIngredient4: null | string
   strIngredient5: null | string
   strIngredient6: null | string
   strIngredient7: null | string
-  strIngredient8: null
-  strIngredient9: null
-  strIngredient10: null
-  strIngredient11: null
-  strIngredient12: null
-  strIngredient13: null
-  strIngredient14: null
-  strIngredient15: null
-  strMeasure1: string
-  strMeasure2: string
-  strMeasure3: string
+  strIngredient8: null | string
+  strIngredient9: null | string
+  strIngredient10: null | string
+  strIngredient11: null | string
+  strIngredient12: null | string
+  strIngredient13: null | string
+  strIngredient14: null | string
+  strIngredient15: null | string
+  strMeasure1: null | string
+  strMeasure2: null | string
+  strMeasure3: null | string
   strMeasure4: null | string
   strMeasure5: null | string
   strMeasure6: null | string
   strMeasure7: null | string
-  strMeasure8: null
-  strMeasure9: null
-  strMeasure10: null
-  strMeasure11: null
-  strMeasure12: null
-  strMeasure13: null
-  strMeasure14: null
-  strMeasure15: null
+  strMeasure8: null | string
+  strMeasure9: null | string
+  strMeasure10: null | string
+  strMeasure11: null | string
+  strMeasure12: null | string
+  strMeasure13: null | string
+  strMeasure14: null | string
+  strMeasure15: null | string
   strImageSource: null | string
   strImageAttribution: null | string
   strCreativeCommonsConfirmed: string
